00a6f6a9f6b0b294718f05ef366e10eec9cf2480
Shashwat Ganesh <shashwatganesh@protonmail.com>
2023-06-29 00:33:25 +0530

    Calculate obstacle distances using range image

--- rs-pcl-color.cpp
+++ rs-pcl-color.cpp
@@ -40,17 +40,7 @@ using pcl_ptr = pcl::PointCloud<pcl::PointXYZ>::Ptr;
 void register_glfw_callbacks(window& app, state& app_state);
 void draw_pointcloud(window& app, state& app_state, const std::vector<pcl_ptr>& points);
 
-void calculate_obstacle_distances(pcl::octree::OctreePointCloudSearch<pcl::PointXYZ>& t, std::vector<float>& distances, float hfov);
-void 
-setViewerPose (pcl::visualization::PCLVisualizer& viewer, const Eigen::Affine3f& viewer_pose)
-{
-Eigen::Vector3f pos_vector = viewer_pose * Eigen::Vector3f(0, 0, 0);
-Eigen::Vector3f look_at_vector = viewer_pose.rotation () * Eigen::Vector3f(0, 0, 1) + pos_vector;
-Eigen::Vector3f up_vector = viewer_pose.rotation () * Eigen::Vector3f(0, -1, 0);
-viewer.setCameraPosition (pos_vector[0], pos_vector[1], pos_vector[2],
-                         look_at_vector[0], look_at_vector[1], look_at_vector[2],
-                         up_vector[0], up_vector[1], up_vector[2]);
-}
+void calculate_obstacle_distances(pcl_ptr pc, std::vector<float>& distances, float fov[2]);
 
 pcl_ptr points_to_pcl(const rs2::points& points)
 {
@@ -190,15 +180,34 @@ catch (const std::exception & e)
     return EXIT_FAILURE;
 }
 
-void calculate_obstacle_distances(pcl::octree::OctreePointCloudSearch<pcl::PointXYZ>& t, std::vector<float>& distances, float hfov)
+void calculate_obstacle_distances(pcl_ptr pc, std::vector<float>& distances, float fov[2])
 {
-    float alpha = hfov / 72.0f;
-    float h = 4.0f;
-    float x = h*std::tan(hfov/2.0f);
-    std::iota(begin(distances), end(distances), 1);
-    std::transform(begin(distances), end(distances), begin(distances), [alpha](float a) { return a*alpha; });
-    std::transform(begin(distances), end(distances), begin(distances), [x, hfov, h](float a) { return x - (h*std::tan((hfov/2.0)-a));
-    });
+    Eigen::Affine3f rs_pose = (Eigen::Affine3f) Eigen::Translation3f(0.0f, 0.0f, 0.0f);
+    float angular_res = (float) (1.0f * (M_PI/180.0f));
+    pcl::RangeImage::CoordinateFrame coord_frame = pcl::RangeImage::CAMERA_FRAME;
+    float noise_lvl = 0.0f;
+    float min_range = 0.0f;
+    int border = 0;
+    pcl::RangeImage rg_img;
+    rg_img.createFromPointCloud(*pc, angular_res, fov[0], fov[1], rs_pose, coord_frame, noise_lvl, min_range, border);
+    float * goods = rg_img.getRangesArray();
+    std::cout<<rg_img<<"\n";
+    std::cout << goods << "\n";
+
+    int rays = distances.size();
+    for (int i = 1; i <= rays; i++) {
+        pcl::PointWithRange ray;
+        int idx = i*(88.0f/72.0f);
+        rg_img.get1dPointAverage(idx, 1, 1, 58, 58, ray);
+        distances[i-1] = ray.range;
+    }
+    // float alpha = hfov / 72.0f;
+    // float h = 4.0f;
+    // float x = h*std::tan(hfov/2.0f);
+    // std::iota(begin(distances), end(distances), 1);
+    // std::transform(begin(distances), end(distances), begin(distances), [alpha](float a) { return a*alpha; });
+    // std::transform(begin(distances), end(distances), begin(distances), [x, hfov, h](float a) { return x - (h*std::tan((hfov/2.0)-a));
+    // });
     std::copy(begin(distances), end(distances), std::ostream_iterator<float>(std::cout, " "));
     std::cout << "\n";
 }
