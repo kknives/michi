e03cbea6879f0a067d4b17e53bfa96da2d1f9f81
Shashwat Ganesh <shashwatganesh@protonmail.com>
2023-07-07 23:19:38 +0530

    Add or_else clause

--- tests/test_realsense_generator.cpp
+++ tests/test_realsense_generator.cpp
@@ -1,8 +1,9 @@
 #include <gtest/gtest.h>
-#include "lib/realsense_generator.hpp"
+#include "realsense_generator.hpp"
+#include <system_error>
 
 TEST(RealsenseGeneratorTest, ReturnsFov) {
-  auto [_, fovh, fovv] = setup_device();
+  auto [_, fovh, fovv] = setup_device().or_else([] (std::error_code e){ FAIL() << e.message(); }).value();
   EXPECT_NE(fovh, 0.0f);
   EXPECT_NE(fovv, 0.0f);
 }
