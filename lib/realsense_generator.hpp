da8e2c9f04843c86734c681185b1bf079865b4fd
Shashwat Ganesh <shashwatganesh@protonmail.com>
2023-07-10 01:23:45 +0530

    Extend try block and check device count

--- lib/realsense_generator.hpp
+++ lib/realsense_generator.hpp
@@ -1,5 +1,7 @@
 #include "cppcoro/async_generator.hpp"
 #include "expected.hpp"
+#include <experimental/coroutine>
+#include <iostream>
 // #include <format>
 #include <librealsense2/hpp/rs_frame.hpp>
 #include <librealsense2/hpp/rs_pipeline.hpp>
@@ -17,23 +19,28 @@ template <typename T>
 using tResult = tl::expected<T, std::error_code>;
 
 auto setup_device() -> tResult<std::tuple<rs2::pipeline, float, float>> {
-  rs2::pipeline pipe;
-  rs2::config stream_config;
-  float fov[2];
   try{
+    rs2::pipeline pipe;
+    rs2::config stream_config;
+    rs2::context ctx;
+    float fov[2];
+
+    auto devices = ctx.query_devices();
+    if (devices.size() == 0) return tl::unexpected{std::make_error_code(std::errc::no_such_device)};
     stream_config.enable_stream(rs2_stream::RS2_STREAM_DEPTH, 0, 424, 240, rs2_format::RS2_FORMAT_Z16, 30);
     rs2::pipeline_profile selection = pipe.start(stream_config);
     auto depth_stream = selection.get_stream(RS2_STREAM_DEPTH).as<rs2::video_stream_profile>();
     auto i = depth_stream.get_intrinsics();
     rs2_fov(&i, fov);
+    fov[0] = (fov[0] * M_PI)/180.0f;
+    fov[1] = (fov[1] * M_PI)/180.0f;
+    return std::tie(pipe, fov[0], fov[1]);
   }
   catch (const std::exception& e) {
+    std::cout << "GOT: " << e.what() << "\n";
     auto error = std::make_error_code(std::errc::no_such_device);
     return tl::unexpected{error};
   }
-  fov[0] = (fov[0] * M_PI)/180.0f;
-  fov[1] = (fov[1] * M_PI)/180.0f;
-  return std::tie(pipe, fov[0], fov[1]);
 }
 
 struct PipelineReady {
