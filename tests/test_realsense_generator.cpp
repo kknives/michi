cfe4275541e4e8d3681c8417309faf92370c4abf
Shashwat Ganesh <shashwatganesh@protonmail.com>
2023-07-09 23:15:18 +0530

    Go back to GTest, patchelf fixes it too

--- tests/test_realsense_generator.cpp
+++ tests/test_realsense_generator.cpp
@@ -1,20 +1,20 @@
-#include <catch2/catch_test_macros.hpp>
+#include <gtest/gtest.h>
 #include "realsense_generator.hpp"
 #include <system_error>
 #include "cppcoro/sync_wait.hpp"
 
-TEST_CASE("A non-zero FOV is returned", "[requires_device]") {
-  auto [pipe, fovh, fovv] = setup_device().or_else([] (std::error_code e){ FAIL( e.message() ); }).value();
-  REQUIRE(fovh > 0.0f);
-  REQUIRE(fovv > 0.0f);
+TEST(RealsenseGeneratorTest, ReturnsFov) {
+  auto [pipe, fovh, fovv] = setup_device().or_else([] (std::error_code e){ FAIL() << e.message(); }).value();
+  EXPECT_NE(fovh, 0.0f);
+  EXPECT_NE(fovv, 0.0f);
   pipe.stop();
 }
 
-TEST_CASE("Generator returns a non-trivial pointset", "[requires_device]") {
-  auto [pipe, fovh, fovv] = setup_device().or_else([] (std::error_code e){ FAIL( e.message() ); }).value();
+TEST(RealsenseGeneratorTest, ReturnNonTrivialPoints) {
+  auto [pipe, fovh, fovv] = setup_device().or_else([] (std::error_code e){ FAIL() << e.message(); }).value();
   auto points_sequence = next_cloud_and_image(pipe);
   for (int i = 0; i < 10; i++) {
     auto [points, frame] = *cppcoro::sync_wait(begin(points_sequence));
-    REQUIRE(points.size() > 0);
+    EXPECT_GT(points.size(), 0);
   }
 }
