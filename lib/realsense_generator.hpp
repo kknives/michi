5f5284e497fb4531e4ef43c6259a7883068ba1be
Shashwat Ganesh <shashwatganesh@protonmail.com>
2023-07-10 22:42:37 +0530

    Implement error_codes for returning errors

--- lib/realsense_generator.hpp
+++ lib/realsense_generator.hpp
@@ -5,6 +5,7 @@
 #include <librealsense2/hpp/rs_frame.hpp>
 #include <librealsense2/hpp/rs_pipeline.hpp>
 #include <librealsense2/hpp/rs_processing.hpp>
+#include <type_traits>
 #include <utility>
 #include <ranges>
 #include <system_error>
@@ -16,8 +17,37 @@
 
 template <typename T>
 using tResult = tl::expected<T, std::error_code>;
+using tl::make_unexpected;
 
-auto setup_device() -> tResult<std::tuple<rs2::pipeline, float, float>> {
+enum class DeviceErrc {
+  // 0 imples success
+  NoDeviceConnected = 10, // Setup error
+  LibrsError = 20, // librealsense gave back an error
+};
+struct DeviceErrCategory : std::error_category {
+  const char* name() const noexcept override {
+    return "DeviceIO";
+  }
+  std::string message(int ev) const override {
+    switch (static_cast<DeviceErrc>(ev)) {
+      case DeviceErrc::NoDeviceConnected:
+      return "no device connected: cannot acquire data";
+      case DeviceErrc::LibrsError:
+      return "failure in librealsense";
+      default:
+      return "(unrecognized error)";
+    }
+  }
+};
+const DeviceErrCategory deverrc_category;
+std::error_code make_error_code(DeviceErrc e) {
+  return {static_cast<int>(e), deverrc_category};
+}
+namespace std {
+  template <>
+  struct is_error_code_enum<DeviceErrc> : true_type {};
+}
+auto setup_device() noexcept -> tResult<std::tuple<rs2::pipeline, float, float>> {
   try{
     rs2::pipeline pipe;
     rs2::config stream_config;
@@ -25,7 +55,7 @@ auto setup_device() -> tResult<std::tuple<rs2::pipeline, float, float>> {
     float fov[2];
 
     auto devices = ctx.query_devices();
-    if (devices.size() == 0) return tl::unexpected{std::make_error_code(std::errc::no_such_device)};
+    if (devices.size() == 0) return make_unexpected(DeviceErrc::NoDeviceConnected);
     stream_config.enable_stream(rs2_stream::RS2_STREAM_DEPTH, 0, 424, 240, rs2_format::RS2_FORMAT_Z16, 30);
     rs2::pipeline_profile selection = pipe.start(stream_config);
     auto depth_stream = selection.get_stream(RS2_STREAM_DEPTH).as<rs2::video_stream_profile>();
@@ -36,9 +66,8 @@ auto setup_device() -> tResult<std::tuple<rs2::pipeline, float, float>> {
     return std::tie(pipe, fov[0], fov[1]);
   }
   catch (const std::exception& e) {
-    std::cout << "GOT: " << e.what() << "\n";
-    auto error = std::make_error_code(std::errc::no_such_device);
-    return tl::unexpected{error};
+    std::cerr << "GOT: " << e.what() << "\n";
+    return make_unexpected(DeviceErrc::LibrsError);
   }
 }
 
