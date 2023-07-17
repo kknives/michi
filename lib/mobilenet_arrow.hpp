b4898b66fcbb89baf593d6925fa699ca67471d91
Shashwat Ganesh <shashwatganesh@protonmail.com>
2023-07-17 16:18:24 +0530

    mobilenet_arrow: wip implementation of classify

---
+++ lib/mobilenet_arrow.hpp
@@ -0,0 +1,78 @@
+#pragma once
+
+#include "classification_model.hpp"
+#include <algorithm>
+#include <iostream>
+#include <librealsense2/rs.hpp>
+#include <numeric>
+#include <onnxruntime_c_api.h>
+#include <onnxruntime_cxx_api.h>
+#include <opencv4/opencv2/dnn/dnn.hpp>
+#include <opencv4/opencv2/opencv.hpp>
+#include <span>
+#include <spdlog/spdlog.h>
+
+const std::array<const char*, 1> MOBILENET_ARROW_INPUT_NAMES{ "input_tensor" };
+const std::array<int64_t, 4> MOBILENET_ARROW_INPUT_SHAPE{ 1, 480, 640, 3 };
+const std::array<const char*, 8> MOBILENET_ARROW_OUTPUT_NAMES{
+  "detection_anchor_indices",    "detection_boxes",     "detection_classes",
+  "detection_multiclass_scores", "detection_scores",    "num_detections",
+  "raw_detection_boxes",         "raw_detection_scores"
+};
+
+template<size_t N>
+constexpr int
+product(const std::span<const std::int64_t, N> v)
+{
+  int total = 1;
+  for (auto& i : v)
+    total *= i;
+  return total;
+}
+
+class MobilenetArrowClassifier
+{
+  Ort::Env m_env;
+  Ort::SessionOptions m_session_options;
+  Ort::Session m_session;
+
+  Ort::AllocatorWithDefaultOptions m_allocator;
+  std::array<Ort::Value, 1> m_input_tensor;
+
+public:
+  MobilenetArrowClassifier(std::string const& s)
+    : m_env(ORT_LOGGING_LEVEL_WARNING, "MobilenetArrowClassifier")
+    , m_session(m_env, s.c_str(), m_session_options)
+    , m_input_tensor{ Ort::Value::CreateTensor<uint8_t>(
+        m_allocator,
+        MOBILENET_ARROW_INPUT_SHAPE.data(),
+        MOBILENET_ARROW_INPUT_SHAPE.size()) }
+  {
+    spdlog::info("Initialized and loaded MobilenetArrow ONNX session");
+  }
+  size_t classify(cv::Mat image)
+  {
+    // Image preprocessing
+    cv::cvtColor(image, image, cv::COLOR_BGR2RGB);
+
+    // Convert to NHWC
+    cv::transpose(image, image);
+    image = image.reshape(
+      1); // Use OpenCV's NCHW behaviour for adding the N "channel"
+    // spdlog::debug("Image after cv processing: {}", image.size); // FIXME
+    assert(image.size.p[0] == MOBILENET_ARROW_INPUT_SHAPE[0] and
+           image.size.p[1] == MOBILENET_ARROW_INPUT_SHAPE[1] and
+           image.size.p[2] == MOBILENET_ARROW_INPUT_SHAPE[2] and
+           image.size.p[3] == MOBILENET_ARROW_INPUT_SHAPE[3]);
+    auto dest = m_input_tensor[0].GetTensorMutableData<uint8_t>();
+    std::copy(image.begin<uint8_t>(), image.end<uint8_t>(), dest);
+
+    auto output_tensors = m_session.Run(Ort::RunOptions{ nullptr },
+                                        MOBILENET_ARROW_INPUT_NAMES.data(),
+                                        m_input_tensor.data(),
+                                        MOBILENET_ARROW_INPUT_NAMES.size(),
+                                        MOBILENET_ARROW_OUTPUT_NAMES.data(),
+                                        MOBILENET_ARROW_OUTPUT_NAMES.size());
+    // TODO: Complete this
+  }
+};
