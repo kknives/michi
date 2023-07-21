a7051e85b4e5ab3560da63c20f8b0c00f4b1eed9
Shashwat Ganesh <shashwatganesh@protonmail.com>
2023-07-21 18:24:31 +0530

    state: require object_in_view affordance

--- lib/state.hpp
+++ lib/state.hpp
@@ -1,16 +1,28 @@
 #pragma once
 
 #include <memory>
+#include <optional>
 
+enum class ObjectType {
+  ARROW_LEFT,
+  ARROW_RIGHT,
+  CONE,
+};
+
+using tObjectSpec = std::tuple<ObjectType, float, float>;
 class State {
   private:
   struct dState {
     virtual ~dState() {}
+    virtual std::optional<tObjectSpec> internal_object_in_view() = 0;
   };
 
   template <typename T>
   struct cState : public dState {
     cState(T&& t) : m_value(std::move(t)) {}
+    auto internal_object_in_view() -> std::optional<tObjectSpec> override {
+      return state_object_in_view(m_value);
+    }
     T m_value;
   };
 
@@ -18,4 +30,7 @@ class State {
   public:
   template <typename T>
   State(T t) : m_value{new cState<T>(std::move(t))} {}
+  friend std::optional<tObjectSpec> object_in_view(State& state) {
+    return state.m_value->internal_object_in_view();
+  }
 };
