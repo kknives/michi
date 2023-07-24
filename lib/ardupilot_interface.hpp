ff99f24b3ba7a0698d6e0d261ebbe6b00c2ec20a
Shashwat Ganesh <shashwatganesh@protonmail.com>
2023-07-24 10:24:55 +0530

    ardupilot_interface: clang-format

--- lib/ardupilot_interface.hpp
+++ lib/ardupilot_interface.hpp
@@ -2,62 +2,70 @@
 
 #include "expected.hpp"
 #include "mavlink_types.h"
-#include <spdlog/spdlog.h>
 #include <algorithm>
 #include <asio.hpp>
-#include <asio/serial_port.hpp>
 #include <asio/experimental/as_tuple.hpp>
 #include <asio/experimental/awaitable_operators.hpp>
+#include <asio/serial_port.hpp>
 #include <asio/write.hpp>
 #include <chrono>
 #include <cmath>
 #include <mavlink/common/mavlink.h>
 #include <memory>
 #include <span>
+#include <spdlog/spdlog.h>
 
-enum class MavlinkErrc {
+enum class MavlinkErrc
+{
   NoHeartbeat = 1, // System Failure
   FailedWrite,
   FailedRead,
   TransmitTimeout = 10, // Timeouts
   ReceiveTimeout,
 };
-struct MavlinkErrCategory : std::error_category {
-  const char* name() const noexcept override {
+struct MavlinkErrCategory : std::error_category
+{
+  const char* name() const noexcept override
+  {
     return "AutopilotCommunication";
   }
-  std::string message(int ev) const override {
+  std::string message(int ev) const override
+  {
     switch (static_cast<MavlinkErrc>(ev)) {
       case MavlinkErrc::NoHeartbeat:
-      return "no heartbeat received from autopilot";
+        return "no heartbeat received from autopilot";
       case MavlinkErrc::FailedWrite:
-      return "could not write, asio error";
+        return "could not write, asio error";
       case MavlinkErrc::FailedRead:
-      return "could not read, asio error";
+        return "could not read, asio error";
       case MavlinkErrc::ReceiveTimeout:
-      return "did not get response, timed out";
+        return "did not get response, timed out";
       case MavlinkErrc::TransmitTimeout:
-      return "could not send message, timed out";
+        return "could not send message, timed out";
       default:
-      return "(unrecognized error)";
+        return "(unrecognized error)";
     }
   }
 };
 const MavlinkErrCategory mavlinkerrc_category;
-std::error_code make_error_code(MavlinkErrc e) {
-  return {static_cast<int>(e), mavlinkerrc_category};
+std::error_code
+make_error_code(MavlinkErrc e)
+{
+  return { static_cast<int>(e), mavlinkerrc_category };
 }
 namespace std {
-  template <>
-  struct is_error_code_enum<MavlinkErrc> : true_type {};
+template<>
+struct is_error_code_enum<MavlinkErrc> : true_type
+{};
 }
 
-template <typename T>
+template<typename T>
 using tResult = tl::expected<T, std::error_code>;
 using tl::make_unexpected;
 using namespace std::chrono;
 using namespace asio::experimental::awaitable_operators;
-constexpr auto use_nothrow_awaitable = asio::experimental::as_tuple(asio::use_awaitable);
+constexpr auto use_nothrow_awaitable =
+  asio::experimental::as_tuple(asio::use_awaitable);
 const float INVALID = 0.0f;
 
 class MavlinkInterface
@@ -79,23 +87,30 @@ class MavlinkInterface
   uint32_t USE_VELOCITY = 0x0DE7;
   uint32_t USE_YAW = 0x09FF;
 
-  inline auto get_uptime() -> uint32_t {
+  inline auto get_uptime() -> uint32_t
+  {
     return duration_cast<milliseconds>(steady_clock::now() - m_start).count();
   }
-  auto send_message(const mavlink_message_t& msg) -> asio::awaitable<std::tuple<asio::error_code, std::size_t>> {
+  auto send_message(const mavlink_message_t& msg)
+    -> asio::awaitable<std::tuple<asio::error_code, std::size_t>>
+  {
     uint8_t buffer[MAVLINK_MAX_PACKET_LEN];
     auto len = mavlink_msg_to_send_buffer(buffer, &msg);
-    auto ret_tup = co_await asio::async_write(m_uart, asio::buffer(buffer, len), use_nothrow_awaitable);
+    auto ret_tup = co_await asio::async_write(
+      m_uart, asio::buffer(buffer, len), use_nothrow_awaitable);
     co_return ret_tup;
   }
-  auto wait_for_next_message(uint8_t channel) -> asio::awaitable<tResult<void>> {
+  auto wait_for_next_message(uint8_t channel) -> asio::awaitable<tResult<void>>
+  {
     mavlink_status_t* status = mavlink_get_channel_status(channel);
     auto msgs_before = status->msg_received;
 
     std::vector<uint8_t> buffer(8);
     while (status->msg_received == msgs_before) {
-      auto [error, len] = co_await m_uart.async_read_some(asio::buffer(buffer), use_nothrow_awaitable);
-      if (error) co_return make_unexpected(error);
+      auto [error, len] = co_await m_uart.async_read_some(
+        asio::buffer(buffer), use_nothrow_awaitable);
+      if (error)
+        co_return make_unexpected(error);
 
       std::for_each_n(cbegin(buffer), len, [channel, status](const uint8_t c) {
         mavlink_message_t msg;
@@ -106,12 +121,15 @@ class MavlinkInterface
   auto set_guided_mode() {}
   auto arm_autopilot() {}
   auto disarm_autopilot() {}
+
 public:
   MavlinkInterface(asio::serial_port sp)
-    : m_uart{ std::move(sp) }, m_start{steady_clock::now()}
+    : m_uart{ std::move(sp) }
+    , m_start{ steady_clock::now() }
   {
   }
-  auto set_target_position_local(std::span<float, 3> xyz) -> asio::awaitable<tResult<void>>
+  auto set_target_position_local(std::span<float, 3> xyz)
+    -> asio::awaitable<tResult<void>>
   {
     mavlink_message_t msg;
     mavlink_msg_set_position_target_local_ned_pack_chan(
@@ -141,21 +159,31 @@ public:
       co_return make_unexpected(MavlinkErrc::FailedWrite);
     }
   }
-  auto set_target_velocity_local(){}
+  auto set_target_velocity_local() {}
   auto set_target_heading_local() {}
   auto get_position_global() {}
-  auto heartbeat() -> asio::awaitable<tResult<void>>{
+  auto heartbeat() -> asio::awaitable<tResult<void>>
+  {
     mavlink_message_t msg;
-    mavlink_msg_heartbeat_pack_chan(m_system_id, m_my_id, m_heartbeat_channel, &msg,
-    MAV_TYPE_ONBOARD_CONTROLLER, MAV_AUTOPILOT_INVALID, MAV_MODE_FLAG_GUIDED_ENABLED, INVALID, MAV_STATE_STANDBY);
+    mavlink_msg_heartbeat_pack_chan(m_system_id,
+                                    m_my_id,
+                                    m_heartbeat_channel,
+                                    &msg,
+                                    MAV_TYPE_ONBOARD_CONTROLLER,
+                                    MAV_AUTOPILOT_INVALID,
+                                    MAV_MODE_FLAG_GUIDED_ENABLED,
+                                    INVALID,
+                                    MAV_STATE_STANDBY);
     auto [error, written] = co_await send_message(msg);
     if (error) {
       co_return make_unexpected(MavlinkErrc::FailedWrite);
     }
 
     co_await wait_for_next_message(m_heartbeat_channel);
-    const mavlink_message_t* new_msg = mavlink_get_channel_buffer(m_heartbeat_channel);
-    if (new_msg->msgid != MAVLINK_MSG_ID_HEARTBEAT) co_return make_unexpected(MavlinkErrc::NoHeartbeat);
+    const mavlink_message_t* new_msg =
+      mavlink_get_channel_buffer(m_heartbeat_channel);
+    if (new_msg->msgid != MAVLINK_MSG_ID_HEARTBEAT)
+      co_return make_unexpected(MavlinkErrc::NoHeartbeat);
     spdlog::debug("Got heartbeat");
   }
 };
