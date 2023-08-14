84c6e90254b67f9ad0e50ed20059e3118b9add0c
Shashwat Ganesh <shashwatganesh@protonmail.com>
2023-08-15 01:02:44 +0530

    arrow_planner: use updated MavlinkInterface, set_goal() is async

--- lib/arrow_global_planner.hpp
+++ lib/arrow_global_planner.hpp
@@ -1,27 +1,46 @@
 #pragma once
 
 #include "state.hpp"
+#include <asio/steady_timer.hpp>
+#include <chrono>
 #include <limits>
 #include <cmath>
+#include "ardupilot_interface.hpp"
 
 class ArrowPlanner {
-  float arrow_min_approach;
-  float cone_min_approach;
-  int hold_ms;
+  float m_arrow_min_approach;
+  float m_cone_min_approach;
+  int m_hold_ms;
+  float m_careful_vel_ms = 0.0001;
+  float m_approach_vel_ms = 0.00001;
   public:
-  auto get_goal(State& state) -> void {
+  auto set_goal(MavlinkInterface& mi, State& state) -> asio::awaitable<tResult<void>>{
     auto front = object_in_view(state);
     if (not front.has_value()) {
       // return continue-as-usual goal
+      // set velocity here, if slowed down
+      co_return;
     }
     auto [object, approach, angle] = *front;
     if (object == ObjectType::ARROW_LEFT or object == ObjectType::ARROW_RIGHT) {
       if (std::isinf(approach)) {
         // slow down
-        set_position_target_local({.vel = 0.001});
+        co_await mi.set_target_velocity({m_careful_vel_ms, 0.0f, 0.0f});
       }
-      if (int(approach*1000) > int(arrow_min_approach*1000)) {
-        set_position_target_local({.vel = 0.00001});
+      if (int(approach*1000) > int(m_arrow_min_approach*1000)) {
+        co_await mi.set_target_velocity({m_approach_vel_ms, 0.0f, 0.0f});
+
+        asio::steady_timer timer(co_await asio::this_coro::executor);
+        timer.expires_at(steady_clock::now()+2s);
+        co_await timer.async_wait(use_nothrow_awaitable);
+
+        co_await mi.set_target_velocity({0.0f, 0.0f, 0.0f});
+        timer.expires_at(steady_clock::now()+12s);
+        co_await timer.async_wait(use_nothrow_awaitable);
+
+        // compute rotation quaternion
+        co_await mi.set_target_attitude({0.0f, 0.0f, 0.0f, 0.0f}, 0.5, 0.5);
+
         // slow down even more, prepare to stop
         // stop
         // turn
@@ -31,7 +50,7 @@ class ArrowPlanner {
       // return goal
     }
     if (auto cone = cone_in_view(state); arrows == 5) {
-      set_position_target_local({.x = 0.001});
+      co_await mi.set_target_velocity({m_approach_vel_ms, 0.0f, 0.0f});
       // Approach cone and stop
       // return goal
     }
