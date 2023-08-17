28e16fcfad2645f296740899f90546c53b310a71
Shashwat Ganesh <shashwatganesh@protonmail.com>
2023-08-17 21:48:08 +0530

    ardupilot_interface: update test to use receive_message_loop()

--- tests/test_ardupilot_interface.cpp
+++ tests/test_ardupilot_interface.cpp
@@ -1,15 +1,25 @@
 #include <asio/detached.hpp>
 #include <exception>
 #include <gtest/gtest.h>
+#include <thread>
 #include "ardupilot_interface.hpp"
 
 using namespace std::literals::chrono_literals;
 TEST(ArdupilotInterfaceTest, HeartBeats) {
-  asio::io_context io_ctx;
-  MavlinkInterface mi(asio::serial_port(io_ctx, "/dev/ttyACM0"));
-  asio::co_spawn(io_ctx, mi.init(), asio::detached);
-  for (int i = 0; i < 4; i++) {
-    asio::co_spawn(io_ctx, mi.heartbeat(), [](std::exception_ptr p, tResult<void> r) {
+  asio::io_context io_ctx(2);
+  MavlinkInterface mi(asio::serial_port(io_ctx, "/dev/pts/13"));
+  asio::co_spawn(io_ctx, mi.receive_message_loop(), [](std::exception_ptr p, tResult<void> r) {
+    if (p) {
+      try { std::rethrow_exception(p); }
+      catch(const std::exception& e) {
+        FAIL() << "receive_message_loop coroutine threw exception: " << e.what() << "\n";
+      }
+    }
+    r.map_error([] (std::error_code e) {
+      FAIL() << "receive_message_loop coroutine faced error: " << e.category().name() << ": " << e.message() << "\n";
+    });
+  });
+    asio::co_spawn(io_ctx, heartbeat_loop(mi), [](std::exception_ptr p, tResult<void> r) {
       if (p) {
         try { std::rethrow_exception(p); }
         catch(const std::exception& e) {
@@ -20,6 +30,5 @@ TEST(ArdupilotInterfaceTest, HeartBeats) {
         FAIL() << "Heartbeat coroutine faced error: " << e.category().name() << ": " << e.message() << "\n";
       });
     });
-  }
   io_ctx.run_for(5s);
 }
