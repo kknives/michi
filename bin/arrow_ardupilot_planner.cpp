6b25208c22ce9403e0f3067c6b9af1e2efbacdfd
Shashwat Ganesh <shashwatganesh@protonmail.com>
2023-08-19 17:05:45 +0530

    arrow_ardupilot_planner: stub out mission machine

--- bin/arrow_ardupilot_planner.cpp
+++ bin/arrow_ardupilot_planner.cpp
@@ -21,6 +21,17 @@ const char* banner = R"Banner(
 >=>        >=> >==>    >=>        >=> >==>    >=>        >==>   >==>>>==> >==>  >=> >==>  >=>  >====>   >==>
 )Banner";
 
+using tLocation = std::tuple<double, double, double>;
+struct Target {
+  enum class Type {
+    ARROW_LOCKED,
+    CONE_LOCKED,
+    ARROW_SIGHTED,
+    CONE_SIGHTED,
+  };
+  Type type;
+  std::optional<tLocation> location;
+};
 struct ArrowState {
 };
 
@@ -53,26 +64,51 @@ auto mission() -> asio::awaitable<void> {
     spdlog::error("Couldn't setup realsense device: {}", e.message());
   });
   auto rs_dev = RealsenseDevice(rs_pipe);
-  auto classifier = ClassificationModel(MobilenetArrowClassifier("lib/saved_model_checkpoint4.onnx"));
-  auto mi = MavlinkInterface(asio::serial_port(this_exec, "/dev/pts/13"));
+  auto classifier = ClassificationModel(MobilenetArrowClassifier(args.get("--model")));
+  auto mi = MavlinkInterface(asio::serial_port(this_exec, args.get("ardupilot")));
 
   bool done = false;
-  // std::optional<Target> current_target;
   asio::co_spawn(this_exec, locate_obstacles(rs_dev, mi), asio::detached);
   asio::co_spawn(this_exec, heartbeat_loop(mi), asio::detached);
   asio::co_spawn(this_exec, mi.receive_message_loop(), asio::detached);
 
+  std::optional<Target> current_target;
+  std::vector<Target> visited_targets;
+
   // Mission Loop
   for (;;) {
-    auto rgb_frame = co_await rs_dev.async_get_rgb_frame(this_exec);
-    cv::Mat image(cv::Size(640, 480), CV_8UC3, (void*)rgb_frame.get_data(), cv::Mat::AUTO_STEP);
-    auto object_found = classify(classifier, image);
+    if (not current_target) {
+      // Get the current frame and process it for potential targets
+      auto rgb_frame = co_await rs_dev.async_get_rgb_frame(this_exec);
+      cv::Mat image(cv::Size(640, 480), CV_8UC3, (void*)rgb_frame.get_data(), cv::Mat::AUTO_STEP);
+      auto object_found = classify(classifier, image);
 
-    if (object_found == 1 or object_found == 2) {
-      // Arrow found
+      if (object_found == 1 or object_found == 2) {
+        // Arrow found
+      }
+
+      // If a target is found, check for duplicates against visited_targets
+      // make a new target, if unique
+      // If depth can lock it, set a target on it with AP
+      // Otherwise, reduce velocity and change heading to match target
+
+      // If no target is found, maintain heading and loop
+      continue;
     }
-    // Accept images as input
-    // Check if there's an arrow
+
+    // There's already a target, check target
+    if (current_target->type == Target::Type::ARROW_SIGHTED or current_target->type == Target::Type::CONE_SIGHTED) {
+      // Check if realsense depth can give a lock on the location
+      // Lock the target by setting a target on AP
+      // If not, set the velocity low, match heading and carefully approach target
+      continue;
+    }
+    if (current_target->type == Target::Type::ARROW_LOCKED or current_target->type == Target::Type::CONE_LOCKED) {
+      // Check if the target approach, reduce velocity if needed,
+      // Get the distance to target, cutoff appraoch and wait if needed
+      // Don't continue, await the wait timer instead
+      // Turn and set heading, clear current_target, push it to visited_targets
+    }
   }
 }
 
