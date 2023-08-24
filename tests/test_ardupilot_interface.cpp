b44beecc19fa2e3e6364eb280880e6031c979f46
Shashwat Ganesh <shashwatganesh@protonmail.com>
2023-08-24 22:18:08 +0530

    ardupilot_interface: adapt test for tcp socket

--- tests/test_ardupilot_interface.cpp
+++ tests/test_ardupilot_interface.cpp
@@ -7,7 +7,9 @@
 using namespace std::literals::chrono_literals;
 TEST(ArdupilotInterfaceTest, HeartBeats) {
   asio::io_context io_ctx(2);
-  MavlinkInterface mi(asio::serial_port(io_ctx, "/dev/pts/13"));
+  tcp::socket sitl(io_ctx);
+  sitl.connect(*tcp::resolver(io_ctx).resolve("0.0.0.0", "5760", tcp::resolver::passive));
+  MavlinkInterface mi(std::move(sitl));
   asio::co_spawn(io_ctx, mi.receive_message_loop(), [](std::exception_ptr p, tResult<void> r) {
     if (p) {
       try { std::rethrow_exception(p); }
@@ -30,5 +32,16 @@ TEST(ArdupilotInterfaceTest, HeartBeats) {
         FAIL() << "Heartbeat coroutine faced error: " << e.category().name() << ": " << e.message() << "\n";
       });
     });
+  asio::co_spawn(io_ctx, mi.set_guided_mode_armed(), [](std::exception_ptr p, tResult<void> r) {
+    if (p) {
+      try { std::rethrow_exception(p); }
+      catch(const std::exception& e) {
+        FAIL() << "Set guided mode coroutine threw exception: " << e.what() << '\n';
+      }
+    }
+    r.map_error([](std::error_code e) {
+      FAIL() << "Set guided mode coroutine faced error: " << e.category().name() << ": " << e.message() << "\n";
+    });
+  });
   io_ctx.run_for(5s);
 }
