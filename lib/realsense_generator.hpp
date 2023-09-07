0d81a12907f8e69c649f037cf008699bdcef656f
Shashwat Ganesh <shashwatganesh@protonmail.com>
2023-09-07 12:04:38 +0530

    realsense_generator: change depth resolution to 640x480

--- lib/realsense_generator.hpp
+++ lib/realsense_generator.hpp
@@ -67,7 +67,7 @@ auto setup_device() noexcept -> tResult<std::tuple<rs2::pipeline, float, float>>
 
     auto devices = ctx.query_devices();
     if (devices.size() == 0) return make_unexpected(DeviceErrc::NoDeviceConnected);
-    stream_config.enable_stream(rs2_stream::RS2_STREAM_DEPTH, 0, 424, 240, rs2_format::RS2_FORMAT_Z16, 30);
+    stream_config.enable_stream(rs2_stream::RS2_STREAM_DEPTH, 0, 640, 480, rs2_format::RS2_FORMAT_Z16, 30);
     stream_config.enable_stream(rs2_stream::RS2_STREAM_COLOR); // Choose resolution here
     rs2::pipeline_profile selection = pipe.start(stream_config);
     auto depth_stream = selection.get_stream(RS2_STREAM_DEPTH).as<rs2::video_stream_profile>();
