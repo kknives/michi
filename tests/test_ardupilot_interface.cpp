3e24082f488960f2f0b56c6ea0ef377975edc92f
Shashwat Ganesh <shashwatganesh@protonmail.com>
2023-09-17 23:28:11 +0530

    ardupilot_interface: update tests to use loop

--- tests/test_ardupilot_interface.cpp
+++ tests/test_ardupilot_interface.cpp
@@ -10,7 +10,7 @@ TEST(ArdupilotInterfaceTest, HeartBeats) {
   tcp::socket sitl(io_ctx);
   sitl.connect(*tcp::resolver(io_ctx).resolve("0.0.0.0", "5760", tcp::resolver::passive));
   MavlinkInterface mi(std::move(sitl));
-  asio::co_spawn(io_ctx, mi.receive_message_loop(), [](std::exception_ptr p, tResult<void> r) {
+  asio::co_spawn(io_ctx, mi.loop(), [](std::exception_ptr p, tResult<void> r) {
     if (p) {
       try { std::rethrow_exception(p); }
       catch(const std::exception& e) {
@@ -21,27 +21,13 @@ TEST(ArdupilotInterfaceTest, HeartBeats) {
       FAIL() << "receive_message_loop coroutine faced error: " << e.category().name() << ": " << e.message() << "\n";
     });
   });
-    asio::co_spawn(io_ctx, heartbeat_loop(mi), [](std::exception_ptr p, tResult<void> r) {
-      if (p) {
-        try { std::rethrow_exception(p); }
-        catch(const std::exception& e) {
-          FAIL() << "Heartbeat coroutine threw exception: " << e.what() << "\n";
-        }
-      }
-      r.map_error([] (std::error_code e) {
-        FAIL() << "Heartbeat coroutine faced error: " << e.category().name() << ": " << e.message() << "\n";
-      });
-    });
-  asio::co_spawn(io_ctx, mi.set_guided_mode_armed(), [](std::exception_ptr p, tResult<void> r) {
+  asio::co_spawn(io_ctx, mi.set_guided_mode_armed(), [](std::exception_ptr p) {
     if (p) {
       try { std::rethrow_exception(p); }
       catch(const std::exception& e) {
         FAIL() << "Set guided mode coroutine threw exception: " << e.what() << '\n';
       }
     }
-    r.map_error([](std::error_code e) {
-      FAIL() << "Set guided mode coroutine faced error: " << e.category().name() << ": " << e.message() << "\n";
-    });
   });
   io_ctx.run_for(5s);
 }
