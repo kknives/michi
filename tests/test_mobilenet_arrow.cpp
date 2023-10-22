4e3abcfc324c887b9a586115af565c1098f136be
Shashwat Ganesh <shashwatganesh@protonmail.com>
2023-10-23 02:06:10 +0530

    mobilenet_arrow: update tests

--- tests/test_mobilenet_arrow.cpp
+++ tests/test_mobilenet_arrow.cpp
@@ -4,19 +4,18 @@
 #include <opencv4/opencv2/opencv.hpp>
 
 TEST(test_mobilenet_arrow, ProcessesImageFrameCorrectly) {
-  auto mac = ClassificationModel(MobilenetArrowClassifier("lib/saved_model_checkpoint4.onnx"));
-  cv::Mat image = cv::imread("tests/sample_left_arrow.jpg");
-  EXPECT_EQ(classify(mac, image), 1);
+  auto mac = ClassificationModel(MobilenetArrowClassifier::make_waseem2_model("lib/w_model2.onnx"));
+  cv::Mat image = cv::imread("tests/sample_right_arrow.jpg");
+  EXPECT_EQ(classify(mac, image, 0.3), ClassificationModel::Detection::ARROW_RIGHT);
 
-  // FIXME
-  // cv::Mat image2 = cv::imread("tests/sample_right_arrow.jpg");
-  // EXPECT_EQ(classify(mac, image2), 2);
+  cv::Mat image2 = cv::imread("tests/sample_left_arrow.jpg");
+  EXPECT_EQ(classify(mac, image2, 0.3), ClassificationModel::Detection::ARROW_LEFT);
 }
 
 TEST(test_mobilenet_arrow, ReturnsBoundingBox) {
-  auto mac = ClassificationModel(MobilenetArrowClassifier("lib/saved_model_checkpoint4.onnx"));
+  auto mac = ClassificationModel(MobilenetArrowClassifier::make_waseem2_model("lib/w_model2.onnx"));
   cv::Mat image = cv::imread("tests/sample_left_arrow.jpg");
-  EXPECT_EQ(classify(mac, image), 1);
+  EXPECT_EQ(classify(mac, image, 0.3), ClassificationModel::Detection::ARROW_LEFT);
   auto bb = get_bounding_box(mac);
   EXPECT_GT(bb.size(), 0);
   EXPECT_LT(bb[0]*480, 480);
