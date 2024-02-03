41e31aaced7704bf1f9d93076d4295c75805bcf0
Shashwat Ganesh <shashwatganesh@protonmail.com>
2024-02-03 13:34:07 +0530

    cmake: remove version tracking module

--- cmake/cmake-git-version-tracking/tests/interfaces/c99/main.c
+++
@@ -1,7 +0,0 @@
-#include <git.h>
-#include <stdio.h>
-
-int main() {
-    printf("%s\n", git_CommitSHA1());
-    return 0;
-}
--- cmake/cmake-git-version-tracking/tests/interfaces/cxx98_cxx17/main.cc
+++
@@ -1,7 +0,0 @@
-#include <git.h>
-#include <iostream>
-
-int main() {
-    std::cout << git::CommitSHA1() << std::endl;
-    return 0;
-}
