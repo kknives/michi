98635192b60e6af66a5276b83e97c9887cf8be3c
Akash <akashchintaluriofficial@gmail.com>
2024-02-15 19:45:45 +0530

    test_aruco_detector.cpp : added sample photo to directory and fixed test

--- tests/test_aruco_detector.cpp
+++ tests/test_aruco_detector.cpp
@@ -1,24 +1,42 @@
 #include <gtest/gtest.h>
-#include <opencv2/opencv.hpp>
 #include "aruco_detector.hpp"
 
-TEST(ArucoDetectorTest, DetectArucoMarkerID42) {
-cv::Mat testImage = cv::imread("sample_aruco_42.jpg");
+#include <gtest/gtest.h>
+#include "aruco_detector.hpp"
 
-int dictionaryType = cv::aruco::DICT_4X4_50;
-Aruco_Detector arucoDetector(testImage, dictionaryType);
+#include <gtest/gtest.h>
+#include "aruco_detector.hpp"
 
-auto detection = arucoDetector.detectAruco(0.6f);
+TEST(ArucoDetectorTest, TestGetPose) {
+    ArucoParams params;
+    ArucoDetector detector(params);
 
-ASSERT_FALSE(detection.empty());
+    cv::Mat image = cv::imread("tests/sample_aruco_42.jpg");
+    std::cout << "Image size: " << image.size().width << "x" << image.size().height << std::endl;
+    bool detection_success = detector.detect_markers(image);
 
-bool markerId42Detected = false;
-for (const auto& marker : detection) {
-if (marker.id == 42) {
-markerId42Detected = true;
-break;
+    EXPECT_TRUE(detection_success);
+    if (detection_success) {
+        auto pose = detector.get_pose();
+
+        cv::Vec3d RVEC = {3.14801, 0.0564981, -0.0176922};
+        cv::Vec3d TVEC = {0.5484, -0.055659, 0.911743};
+
+        EXPECT_NEAR(pose.first[0], RVEC[0], 1e-5);
+        EXPECT_NEAR(pose.second[0], TVEC[0], 1e-5);
+    }
 }
-}
 
-ASSERT_TRUE(markerId42Detected);
+TEST(ArucoDetectorTest, TestDetectedId) {
+    ArucoParams params;
+    ArucoDetector detector(params);
+
+    cv::Mat test_image = cv::imread("tests/sample_aruco_42.jpg");
+    bool detection_success = detector.detect_markers(test_image);
+
+    EXPECT_TRUE(detection_success);
+    if (detection_success) {
+        auto detection_result = detector.get_detection_result();
+        EXPECT_EQ(detection_result.ids[0], 42);
+    }
 }
